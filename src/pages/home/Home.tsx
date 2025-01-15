c2ae3a9055b4be28f98b6ca1cff1a626b7679a16
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-01-15 09:13:12 -0300

    :art: refactor: update Home component to use Tailwind CSS for styling

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,27 +3,13 @@
 const Home = () => {
     return (
         <>
-            <section style={{
-                width: "100vw",
-                display: "flex",
-                justifyContent: "center",
-            }}>
+            <section className="bg-gray-100 max-w-full flex flex-col">
                 <div>
-                    <div style={{
-                        width:"80vw",
-                        display: "flex",
-                        flexDirection: "column",
-                        alignItems: "center",
-                    }}>
+                    <div className="max-w-7xl flex flex-col items-center mx-auto px-4 sm:px-6 lg:px-8">
                         <h2> Seja Bem-vindo(a)</h2>
                         <p> Expresse aqui seus pensamentos e opniões</p>
                     </div>
-                    <div style={{
-                        width:"80vw",
-                        display: "flex",
-                        flexDirection: "column",
-                        alignItems: "center",
-                    }}>
+                    <div className="max-w-7xl flex flex-col items-center mx-auto px-4 sm:px-6 lg:px-8">
                         <img src="https://i.imgur.com/VpwApCU.png" alt="Imagem da Página Home" width="400px" />
                     </div>
                 </div>
