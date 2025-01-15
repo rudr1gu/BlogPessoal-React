8173bc1566831569778d41b169ee7bf32c5212bd
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-01-15 12:00:47 -0300

    :art: feat: add Footer component and update App layout; enhance Home section styling

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,17 +3,34 @@
 const Home = () => {
     return (
         <>
-            <section className="bg-gray-100 w-screen flex flex-col">
-                <div>
-                    <div className="max-w-7xl flex flex-col items-center mx-auto px-4 sm:px-6 lg:px-8">
-                        <h2> Seja Bem-vindo(a)</h2>
-                        <p> Expresse aqui seus pensamentos e opniões</p>
+            <div className="bg-indigo-900 flex justify-center">
+                <div className='container grid grid-cols-2 text-white'>
+                    <div className="flex flex-col gap-4 items-center justify-center py-4">
+                        <h2 className='text-5xl font-bold text-white'>
+                            Seja Bem Vindo(a)!
+                        </h2>
+                        <p className='text-xl'>
+                            Expresse aqui seus pensamentos e opniões
+                        </p>
+
+                        <div className="flex justify-around gap-4">
+                            <div className='rounded text-white 
+                                        border-white border-solid border-2 py-2 px-4'
+                            >
+                                Nova Postagem
+                            </div>
+                        </div>
                     </div>
-                    <div className="max-w-7xl flex flex-col items-center mx-auto px-4 sm:px-6 lg:px-8">
-                        <img src="https://i.imgur.com/VpwApCU.png" alt="Imagem da Página Home" width="400px" />
+
+                    <div className="flex justify-center ">
+                        <img
+                            src="https://i.imgur.com/fyfri1v.png"
+                            alt="Imagem Página Home"
+                            className='w-2/3'
+                        />
                     </div>
                 </div>
-            </section>
+            </div>
         </>
     );
 };
