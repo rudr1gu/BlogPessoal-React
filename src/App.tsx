11ede09ca1a08cfe19f5a2812ebbacfaf9e838d2
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-01-16 19:48:44 -0300

    :sparkles: feat: wrap App component with AuthProvider for authentication context

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,25 @@ import Navbar from './components/navbar/Navbar';
 import Home from './pages/home/Home'
 import Cadastro from './pages/cadastar/Cadastro';
 import Login from './pages/login/Login';
+import { AuthProvider } from './contexts/AuthContext';
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-
-        <Navbar />
-        <main className="min-h-[80vh]">
-          <Routes>
-            <Route path='/' element={<Login />} />
-            <Route path='/home' element={<Home />} />
-            <Route path='/cadastro' element={<Cadastro />} />
-            <Route path='/login' element={<Login />} />
-          </Routes>
-        </main>
-        <Footer />
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
+          <main className="min-h-[80vh]">
+            <Routes>
+              <Route path='/' element={<Login />} />
+              <Route path='/home' element={<Home />} />
+              <Route path='/cadastro' element={<Cadastro />} />
+              <Route path='/login' element={<Login />} />
+            </Routes>
+          </main>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
     </>
 
   );
