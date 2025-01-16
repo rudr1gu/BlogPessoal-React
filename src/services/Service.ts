1d5ceeabf0f5863180d5e62531fb54bd3d12ad6e
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-01-16 14:55:38 -0300

    :sparkles: feat: add Service module for user registration and login with axios integration

---
+++ src/services/Service.ts
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'https://blogpessoal-h068.onrender.com/',
+});
+
+export const cadastrarUsuario = async(url: string, dados: Object, setDados: Function) => {
+    const resposta = await api.post(url, dados);
+    setDados(resposta.data);
+}
+
+export const logarUsuario = async(url: string, dados: Object, setDados: Function) => {
+    const resposta = await api.post(url, dados);
+    setDados(resposta.data);
+}
+
