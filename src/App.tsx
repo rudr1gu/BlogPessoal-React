20b318bde689a94c0514c552ed68d9e25573ca72
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-01-16 11:18:24 -0300

    :art: feat: implement Cadastro page with form and styling; integrate routing in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css'
 import Footer from './components/footer/Footer';
 import Navbar from './components/navbar/Navbar';
 import Home from './pages/home/Home'
+import Cadastro from './pages/cadastar/Cadastro';
 
 function App() {
   return (
     <>
-     <Navbar />
-     <Home />
-     <Footer />
+      <BrowserRouter>
+
+        <Navbar />
+        <main className="min-h-[80vh]">
+          <Routes>
+            <Route path='/home' element={<Home />} />
+            <Route path='/' element={<Cadastro />} />
+          </Routes>
+        </main>
+        <Footer />
+      </BrowserRouter>
     </>
-   
+
   );
 }
 
---
+++ src/pages/cadastar/Cadastro.tsx
@@ -0,0 +1,81 @@
+import './Cadastro.css';
+
+const Cadastrar = () => {
+    return (
+        <>
+            <section className="grid grid-cols-1 lg:grid-cols-2 h-screen 
+              place-items-center font-bold">
+                <div className="fundoCadastro hidden lg:block"></div>
+                <form className='flex justify-center items-center flex-col w-2/3 gap-3' >
+                    <h2 className='text-slate-900 text-5xl'>Cadastrar</h2>
+                    <div className="flex flex-col w-full input">
+                        <input
+                            type="text"
+                            id="nome"
+                            name="nome"
+                            className="border-2 border-slate-700 rounded-md p-2"
+                            required
+                        />
+                        <label htmlFor="nome"><i className='bx bxs-user-circle'></i> Nome</label>
+                    </div>
+                    <div className="flex flex-col w-full input">
+                        <input
+                            type="text"
+                            id="usuario"
+                            name="usuario"
+                            className="border-2 border-slate-700 rounded-md p-2"
+                            required
+                        />
+                        <label htmlFor="usuario"><i className='bx bxs-id-card'></i> Usuario</label>
+                    </div>
+                    <div className="flex flex-col w-full input">
+                        <input
+                            type="text"
+                            id="foto"
+                            name="foto"
+                            className="border-2 border-slate-700 rounded-md p-2"
+                            required
+                        />
+                        <label htmlFor="foto"><i className='bx bxs-image-add'></i> Foto</label>
+                    </div>
+                    <div className="flex flex-col w-full input">
+                        <input
+                            type="password"
+                            id="senha"
+                            name="senha"
+                            className="border-2 border-slate-700 rounded-md p-2"
+                            required
+                        />
+                        <label htmlFor="senha"><i className='bx bx-key' ></i> Senha</label>
+                    </div>
+                    <div className="flex flex-col w-full input">
+                        <input
+                            type="password"
+                            id="confirmarSenha"
+                            name="confirmarSenha"
+                            className="border-2 border-slate-700 rounded-md p-2 shadow-md"
+                            required
+                        />
+                        <label htmlFor="confirmarSenha"> <i className='bx bx-key' ></i> Confirmar Senha</label>
+                    </div>
+                    <div className="flex justify-around w-full gap-8">
+                        <button className='rounded-full text-white bg-red-500 
+                    hover:bg-red-700 w-1/2 py-2' >
+                            Cancelar
+                        </button>
+                        <button
+                            type='submit'
+                            className='rounded-full text-white bg-sky-400 
+                             hover:bg-sky-900 w-1/2 py-2
+                             flex justify-center'
+                        >
+                            Cadastrar
+                        </button>
+                    </div>
+                </form>
+            </section>
+        </>
+    );
+}
+
+export default Cadastrar;
