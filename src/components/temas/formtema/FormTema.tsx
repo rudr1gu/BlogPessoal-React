5f21b2bc9a9914ddc484fae6d7ef68a1c9d02408
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-01-20 10:44:41 -0300

    :sparkles: feat: update FormTema button styling; add methods for creating and updating themes in TemaService

--- src/components/temas/formtema/FormTema.tsx
+++ src/components/temas/formtema/FormTema.tsx
@@ -16,8 +16,8 @@ const FormTema = () => {
                     />
                 </div>
                 <button
-                    className="rounded text-slate-100 bg-indigo-400 
-                               hover:bg-indigo-800 w-1/2 py-2 mx-auto flex justify-center"
+                    className="rounded text-slate-100 bg-sky-400 
+                               hover:bg-sky-800 w-1/2 py-2 mx-auto flex justify-center"
                     type="submit">
                     Cadastrar
                 </button>
--- src/services/TemaService.ts
+++ src/services/TemaService.ts
@@ -6,6 +6,16 @@ class TemaService {
         const resposta = await baseApi.get(url, header);
         setDados(resposta.data);
     }
+
+    cadastrarTema = async (url: string, dados: Object, setDados: Function, header: Object) => {
+        const resposta = await baseApi.post(url, dados, header);
+        setDados(resposta.data);
+    }
+
+    atualizarTema = async (url: string, dados: Object, setDados: Function, header: Object) => {
+        const resposta = await baseApi.put(url, dados, header);
+        setDados(resposta.data);
+    }
 }
 
 export default TemaService;
