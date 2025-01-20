664c8de50f6043fc1b5c7642abe9e397a4858864
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-01-20 11:11:21 -0300

    :sparkles: feat: enhance FormTema for theme creation and editing; add loading state and dynamic button text

--- src/components/temas/formtema/FormTema.tsx
+++ src/components/temas/formtema/FormTema.tsx
@@ -1,11 +1,103 @@
+import { ChangeEvent, useContext, useEffect, useState } from "react";
+import Tema from "../../../models/Tema";
+import TemaService from "../../../services/TemaService";
+import { AuthContext } from "../../../contexts/AuthContext";
+import { useNavigate, useParams } from "react-router-dom";
+import { RotatingLines } from "react-loader-spinner";
+
 const FormTema = () => {
+
+    const temaService = new TemaService();
+    const navigate = useNavigate();
+    const [tema, setTema] = useState<Tema>({} as Tema);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+
+    const { usuario, handleLogout } = useContext(AuthContext);
+    const token = usuario.token;
+
+    const { id } = useParams<{ id: string }>();
+
+    const buscarPorId = async (id: string) => {
+        try {
+            await temaService.buscarTemas(`/temas/${id}`, setTema, {
+                headers: { Authorization: token }
+            });
+        } catch (error: any) {
+            if (error.toString().includes('403')) {
+                handleLogout();
+            }
+        }
+    }
+
+    useEffect(() => {
+        if (token === '') {
+            alert('Você precisa estar logado!');
+            navigate('/');
+        }
+    }, [token]);
+
+    useEffect(() => {
+        if (id !== undefined) {
+            buscarPorId(id);
+        }
+    }, [id]);
+
+    const atualizarEstado = (e: ChangeEvent<HTMLInputElement>) => {
+        setTema({
+            ...tema,
+            [e.target.name]: e.target.value
+        });
+    }
+
+    const retornar = () => {
+        navigate('/tema');
+    }
+
+    const gerarNovoTema = async (e: ChangeEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setIsLoading(true);
+
+        if (id !== undefined) {
+            try {
+                await temaService.atualizarTema('/temas', tema, setTema, {
+                    headers: { Authorization: token }
+                })
+                alert('Tema atualizado com sucesso!');
+            } catch (error: any) {
+                if (error.toString().includes('403')) {
+                    handleLogout();
+                } else {
+                    alert('Erro ao atualizar o tema!');
+                }
+            }
+        } else {
+            try {
+                await temaService.cadastrarTema('/temas', tema, setTema, {
+                    headers: { Authorization: token }
+                });
+                alert('Tema cadastrado com sucesso!');
+            } catch (error: any) {
+                if (error.toString().includes('403')) {
+                    handleLogout();
+                } else {
+                    alert('Erro ao cadastrar o tema!');
+                }
+            }
+
+        }
+
+        setIsLoading(false);
+        retornar();
+    }
+
+
     return (
         <section className="container flex flex-col items-center justify-center mx-auto">
             <h1 className="text-4xl text-center my-8">
-                Cadastrar Tema
+                {id === undefined ? 'Cadastrar Tema' : 'Editar Tema'}
             </h1>
 
-            <form className="w-1/2 flex flex-col gap-4" >
+            <form className="w-1/2 flex flex-col gap-4" onSubmit={gerarNovoTema}>
                 <div className="flex flex-col gap-2">
                     <label htmlFor="descricao">Descrição do Tema</label>
                     <input
@@ -13,13 +105,25 @@ const FormTema = () => {
                         placeholder="Descreva aqui seu tema"
                         name='descricao'
                         className="border-2 border-slate-700 rounded p-2"
+                        value={tema.descricao}
+                        onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
                     />
                 </div>
                 <button
                     className="rounded text-slate-100 bg-sky-400 
-                               hover:bg-sky-800 w-1/2 py-2 mx-auto flex justify-center"
+                           hover:bg-sky-800 w-1/2 py-2 mx-auto flex justify-center"
                     type="submit">
-                    Cadastrar
+                    {isLoading ?
+                        <RotatingLines
+                            strokeColor="white"
+                            strokeWidth="5"
+                            animationDuration="0.75"
+                            width="24"
+                            visible={true}
+                        /> :
+                        <span>{id === undefined ? 'Cadastrar' : 'Atualizar'}</span>
+
+                    }
                 </button>
             </form>
         </section>
