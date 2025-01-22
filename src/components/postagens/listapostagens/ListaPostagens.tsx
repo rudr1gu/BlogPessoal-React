862ab73066c283c8f3292336baf1019358f57d65
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-01-22 10:15:11 -0300

    :sparkles: feat: add ListaPostagens component to display user posts

---
+++ src/components/postagens/listapostagens/ListaPostagens.tsx
@@ -0,0 +1,15 @@
+import CardPostagens from "../cardpostagens/CardPostagens";
+
+const ListaPostagens = () => {
+    return (
+        <>
+            <div className='container mx-auto my-4 
+                grid grid-cols-1 md:grid-cols-2 
+            	lg:grid-cols-3 gap-4'>
+                <CardPostagens />
+            </div>
+        </>
+    );
+}
+
+export default ListaPostagens;
