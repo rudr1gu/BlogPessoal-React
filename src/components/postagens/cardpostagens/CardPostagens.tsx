7764e23b4ffa1f1db0e1116dc357de7747d62d4a
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-01-22 13:03:02 -0300

    :sparkles: feat: add DeletarPostagem component and integrate with routing for post deletion

--- src/components/postagens/cardpostagens/CardPostagens.tsx
+++ src/components/postagens/cardpostagens/CardPostagens.tsx
@@ -35,9 +35,7 @@ const CardPostagens = ({ postagem }: CardPostagensProps) => {
                     className='w-full text-slate-100 bg-sky-400 hover:bg-sky-800 flex items-center justify-center py-2'>
                     <button>Editar</button>
                 </Link>
-                <Link to=''
-                    className='text-white bg-red-400 
-                    hover:bg-red-700 w-full flex items-center justify-center'>
+                <Link to={`/deletarpostagem/${postagem.id}`} className='text-white bg-red-400 hover:bg-red-700 w-full flex items-center justify-center'>
                     <button>Deletar</button>
                 </Link>
             </div>
