2024569789725015158247a7bff773186ae5bc0a
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-01-22 12:36:12 -0300

    :sparkles: feat: enhance FormPostagem component with loading state and theme selection

--- src/components/temas/listatemas/ListaTemas.tsx
+++ src/components/temas/listatemas/ListaTemas.tsx
@@ -32,7 +32,7 @@ const ListaTemas = () => {
 
     useEffect(() => {
         if (token === '') {
-            handleLogout();
+            alert('Você precisa estar logado')
             navigate('/');
         }
     }, [token]);
