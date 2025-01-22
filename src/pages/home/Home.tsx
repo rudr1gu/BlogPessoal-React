98318b75956ddd29b15d7feeed0a24896312c078
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-01-22 11:54:45 -0300

    :sparkles: feat: integrate ModalPostagem component into Home page for post creation

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,4 +1,5 @@
 import ListaPostagens from "../../components/postagens/listapostagens/ListaPostagens"
+import ModalPostagem from "../../components/postagens/modalpostagem/ModalPostagem";
 
 const Home = () => {
     return (
@@ -14,10 +15,8 @@ const Home = () => {
                         </p>
 
                         <div className="flex justify-around gap-4">
-                            <div className='rounded text-white 
-                                        border-white border-solid border-2 py-2 px-4'
-                            >
-                                Nova Postagem
+                            <div className="flex justify-around gap-4">
+                                <ModalPostagem />
                             </div>
                         </div>
                     </div>
