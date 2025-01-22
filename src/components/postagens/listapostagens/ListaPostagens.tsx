79c678241455d413f0bac7f1a386059134a2eb38
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-01-22 10:15:47 -0300

    :bug: fix: change div to section in ListaPostagens for semantic HTML

--- src/components/postagens/listapostagens/ListaPostagens.tsx
+++ src/components/postagens/listapostagens/ListaPostagens.tsx
@@ -3,11 +3,11 @@ import CardPostagens from "../cardpostagens/CardPostagens";
 const ListaPostagens = () => {
     return (
         <>
-            <div className='container mx-auto my-4 
+            <section className='container mx-auto my-4 
                 grid grid-cols-1 md:grid-cols-2 
             	lg:grid-cols-3 gap-4'>
                 <CardPostagens />
-            </div>
+            </section>
         </>
     );
 }
